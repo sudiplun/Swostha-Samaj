6bd6e176344e8940ebc89db518c0e3154be4f753
sudiplun <sudeeplun@gmail.com>
2023-03-27 17:19:33 +0545

    btn navbar

--- app/src/main/java/com/sudiplun/swosthaSamaj/MainActivity.java
+++ app/src/main/java/com/sudiplun/swosthaSamaj/MainActivity.java
@@ -1,14 +1,46 @@
 package com.sudiplun.swosthaSamaj;
 
+import static com.sudiplun.swosthaSamaj.databinding.ActivityMainBinding.inflate;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.sudiplun.swosthaSamaj.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new homeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new homeFragment());
+                    break;
+                case R.id.person:
+                    replaceFragment(new profileFragment());
+                    break;
+                case R.id.setting:
+                    replaceFragment(new settingsFragment());
+                    break;
+            }
+            return true;
+        });
     }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+
 }
